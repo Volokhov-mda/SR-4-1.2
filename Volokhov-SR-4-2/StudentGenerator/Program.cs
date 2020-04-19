3269f70ad41c8fe4cd97c70a541c44aa834745f6
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-04-19 21:54:58 +0300

    Добавлена библиотека классов Utils с рандомайзером

---
+++ Volokhov-SR-4-2/StudentGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using StudentLibrary;
+
+namespace StudentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ Volokhov-SR-4-2/Utils/utils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Utils
+{
+    public class utils
+    {
+        /// <summary>
+        /// Генератор случайных чисел.
+        /// </summary>
+        public static Random random = new Random();
+    }
+}
