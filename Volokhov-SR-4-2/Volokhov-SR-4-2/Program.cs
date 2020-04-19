7cdafb8aad3a19afc0d8c5524740bf0e03f88ff5
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2020-04-19 20:36:34 +0300

    Init

---
+++ Volokhov-SR-4-2/Volokhov-SR-4-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Volokhov_SR_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
